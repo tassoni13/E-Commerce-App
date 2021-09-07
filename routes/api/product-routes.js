7f66d9f82fe5a1f6ace1d8e19b80503e07b7f9ec
tassoni13 <tassoni13@gmail.com>
2021-09-07 14:33:49 -0400

    Fixed asych on product 'get' routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,22 +4,11 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = Product.findAll({
-      include: [
-        {
-          model: Category
-        },
-        {
-          model: Tag,
-          through: ProductTag,
-          as: 'tags'
-        }
-      ]
-    });
+    const productData = await Product.findAll();
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
@@ -27,21 +16,12 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = Product.findByPk(req.params.id, {
-      include: [
-        {
-          model: Category
-        },
-        {
-          model: Tag,
-          through: ProductTag,
-          as: 'tags'
-        },
-      ]
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Tag, through: ProductTag, as: 'tags'}]
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -123,7 +103,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
 });
 
