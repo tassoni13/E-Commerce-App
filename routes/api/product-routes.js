2d2cb326c56cb3c42150bb863cca163165aeed82
tassoni13 <tassoni13@gmail.com>
2021-09-07 15:00:30 -0400

    Added functions to find all and single tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,9 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll();
+    const productData = await Product.findAll({
+      include: [{model: Category}, {model: Tag, through: ProductTag, as: 'tags'}]
+  });
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
