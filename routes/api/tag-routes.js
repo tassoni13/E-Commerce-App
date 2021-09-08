3e4f5c8599526dcdf8bf836eda618787470f7f45
tassoni13 <tassoni13@gmail.com>
2021-09-07 21:10:15 -0400

    Bug fixes, tested routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -53,7 +53,7 @@ router.put('/:id', async (req, res) => {
       res.status(404).json({message:'No tags found with this id!'});
       return;
     }
-    res.status(200).json(categoryData);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   } 
